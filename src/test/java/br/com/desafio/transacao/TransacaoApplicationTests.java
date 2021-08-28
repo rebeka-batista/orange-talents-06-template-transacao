666dbe4ef1e2813604f506def801795d4dddb4bd
Rebeka Batista <rebekabatista05@outlook.com>
2021-08-28 11:27:27 -0300

    Commit inicial

---
+++ src/test/java/br/com/desafio/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
